1926f1f8a9bd860f5dad21ad754da7dc6e549d9e
Trammell Hudson <hudson@osresearch.net>
2013-12-08 13:16:08 -0500

    pulse on and off, with flashes

---
+++ pulse2/pulse2.ino
@@ -0,0 +1,38 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN		12
+#define NUM_PIXELS	16
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
+	NUM_PIXELS,			// how many pixels on strip?
+	PIN,				// output pin?
+	NEO_GRB + NEO_KHZ800		// type of pixels?
+);
+
+int brightness = 0;
+int direction = 1;
+
+void setup() {
+	pixels.begin();
+}
+
+void loop() {
+	for(int i = 0 ; i < NUM_PIXELS ; i++)
+		pixels.setPixelColor(i, 0, 0, brightness);
+
+	// Make flashes; 10% the time, turn on pixel on full brightness
+	if (random(10) == 0)
+		pixels.setPixelColor(random(0, NUM_PIXELS), 255, 255, 255);
+
+	pixels.show();
+	delay(10);
+
+	if (direction == 1)
+	{
+		if (++brightness == 200)
+			direction = -1;
+	} else {
+		if (--brightness == 0)
+			direction = +1;
+	}
+}
