b720f88eccd8aa83798535dd908c1f4dd6f2dcde
hollyhudson <hollyf@swcp.com>
2013-12-12 20:53:09 -0500

    improved comments

--- fade/fade.ino
+++ fade/fade.ino
@@ -24,24 +24,32 @@ void loop() {
 	// fade all the pixels
 	for(int i = 0 ; i < NUM_PIXELS ; i++)
 	{
+		// Query each pixel on the ring in turn.
+		// For each pixel, grab the numerical value (0-255)
+		// associated with the pixel based on it's 32-bit
+		// representation in the machine.
 		uint32_t c = pixels.getPixelColor(i);
 		int r = (c >> 16) & 0xFF;
 		int g = (c >>  8) & 0xFF;
 		int b = (c >>  0) & 0xFF;
 
+		// Reduce that value by 1/4 intensity (for each pixel).
+		// So that each pixels fades from it's previous setting.
 		r = (r * 3) / 4;
 		g = (g * 3) / 4;
 		b = (b * 3) / 4;
 		pixels.setPixelColor(i, r, g, b);
 	}
 
-	// and now turn on just the one at position
+	// and now turn on the new lead pixel
 	pixels.setPixelColor(position, 0, 128, 0);
 	position = (position + NUM_PIXELS + direction) % NUM_PIXELS;
+
+	// Randomly change direction
 	if (random(100) == 0)
 		direction = -direction;
 
-	// Make flashes; 5% the time, turn on pixel on full brightness
+	// Make a flash; 5% the time, turn on pixel on full brightness
 	if (random(20) == 0)
 		pixels.setPixelColor(random(0, NUM_PIXELS), 255, 255, 255);
 
