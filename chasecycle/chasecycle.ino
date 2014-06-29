3fd99a77f46f3f0c64a37efc2154f259fdb53bd9
Trammell Hudson <hudson@osresearch.net>
2014-06-29 13:38:47 -0400

    use a defined palette for the color wheel

--- chasecycle/chasecycle.ino
+++ chasecycle/chasecycle.ino
@@ -13,21 +13,41 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
 );
 
 
+const uint32_t colors[] = {
+	0x2F0000,
+	0x2F2F00,
+	0x002F00,
+	0x002F4F,
+	0x00002F,
+	0x2F002F,
+};
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t
-Wheel(byte WheelPos) {
-	if(WheelPos < 85) {
-		return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-	} else
-	if(WheelPos < 170) {
-		WheelPos -= 85;
-		return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-	} else {
-		WheelPos -= 170;
-		return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-	}
+Wheel(uint16_t WheelPos) {
+	const unsigned num_colors = sizeof(colors) / sizeof(*colors);
+	unsigned num1 = (WheelPos / 256) % num_colors;
+	unsigned num2 = (num1 + 1) % num_colors;
+	uint32_t c1 = colors[num1];
+	uint32_t c2 = colors[num2];
+	unsigned ratio2 = WheelPos % 256;
+	unsigned ratio1 = 255 - ratio2;
+
+	unsigned r1 = (c1 >> 16) & 0xFF;
+	unsigned g1 = (c1 >>  8) & 0xFF;
+	unsigned b1 = (c1 >>  0) & 0xFF;
+	unsigned r2 = (c2 >> 16) & 0xFF;
+	unsigned g2 = (c2 >>  8) & 0xFF;
+	unsigned b2 = (c2 >>  0) & 0xFF;
+
+	uint32_t r = ((r1 * ratio1 + r2 * ratio2) / 256) & 0xFF;
+	uint32_t g = ((g1 * ratio1 + g2 * ratio2) / 256) & 0xFF;
+	uint32_t b = ((b1 * ratio1 + b2 * ratio2) / 256) & 0xFF;
+
+	return (r << 16) | (g << 8) | (b << 0);
 }
+
 
 int bright[NUM_PIXELS];
 
@@ -47,8 +67,8 @@ unsigned phase;
 unsigned color_phase;
 
 void loop() {
-	delay(20);
-	uint32_t color = Wheel((color_phase++) % 256);
+	delay(10);
+	uint32_t color = Wheel(color_phase += 3);
 
 	for (unsigned i = 0 ; i < NUM_PIXELS ; i++)
 	{
